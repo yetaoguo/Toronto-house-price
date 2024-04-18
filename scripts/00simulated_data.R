01302d46d76db274be4354ca1221aad8c72b9f07
yetaoguo <2011896857@qq.com>
2024-04-18 07:00:30 +0000

    add scripts

---
+++ scripts/00simulated_data.R
@@ -0,0 +1,11 @@
+#simulation
+
+set.seed(1234)
+
+simulated_data <-
+  tibble(
+    Home_Price =rnorm(10,300000,50000),
+    Rent_Bank_Applicants=rbinom(10,40,0.4),
+    Social_Housing_Units=rnorm(10,4,0.1)
+  )
+
